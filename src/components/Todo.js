1018b893f873621aa21fec2a005b54ad842f274e
Harsh Vador <harsh.vador@somaiya.edu>
2022-06-09 12:10:50 +0530

    (refactor): code improvement

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -25,25 +25,27 @@ function Todo({ todos, completeTodo, removeTodo, updateTodo }) {
         completeTodo(id)
     }
  
-    return todos.map((todo, index) => (
+    return <>{todos.map((todo, index) => (
         <div className={todo.isComplete ? 'todo-row complete' : 'todo-row'} key={index}>
             <input type="checkbox" onClick={()=>deleteCheckBox(todo.id)} />
             <div className='todo-content' key={todo.id} onClick={()=>deleteCheckBox(todo.id)}>
                 {todo.text}
             </div>
             <div className='icons'>
-                <RiCloseCircleLine
-                    onClick={() => removeTodo(todo.id)}
-                    className='delete-icon'
-                />
+                
                 <TiEdit
                     onClick={() => setEdit({ id: todo.id, value: todo.text })}
                     className='edit-icon'
                 />
-                
+                <RiCloseCircleLine
+                    onClick={() => removeTodo(todo.id)}
+                    className='delete-icon'
+                />
             </div>
         </div>
-    ));
-};
+    ))}
+    {edit.id?<TodoForm edit={edit} onSubmit={submitUpdate} />:''}
+    </>
+}
 
 export default Todo;
